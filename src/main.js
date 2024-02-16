516bad8f4156a906405f14b7c4cda730978b77ec
wquynguy <wquyenle@gmail.com>
2024-02-16 15:46:09 -0600

    aimplemented toggle functionality for mobile navigation menu

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ $(document).ready(function() {
     $('.fa.fa-bars').click(function(){
         console.log("bars icon clicked");
         $(this).toggleClass('fa-times');
+        $('nav').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load', function () {
+        $('.fa.fa-bars').removeClass('fa-times');
+        $('nav').removeClass('nav-toggle');
     });
 
 });
