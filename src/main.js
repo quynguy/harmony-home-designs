a6b4df3e5271cc47cece53b3e06f02c0c87dedfc
wquynguy <wquyenle@gmail.com>
2024-02-16 17:54:22 -0600

    add animation for counting numbers in elements with the 'count' class

--- src/main.js
+++ src/main.js
@@ -13,4 +13,22 @@ $(document).ready(function() {
         $('nav').removeClass('nav-toggle');
     });
 
+    $('.count').each(function(){
+        var $this = $(this),
+        countTo = $this.attr('data-count');
+        $({ countNum : $this.text() }).animate({
+            countNum : countTo
+        },
+        {
+            duration: 5000,
+            step: function(){
+                $this.text(Math.floor(this.countNum));
+        },
+            complete : function(){
+                $this.text(this.countNum + '+');
+        }
+    }
+        )
+
+    })
 });
